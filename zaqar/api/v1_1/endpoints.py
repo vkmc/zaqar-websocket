a93c460a28e541b5cc8b425d5fb4d69e78ab9f4b
Victoria Martinez de la Cruz <victoria@redhat.com>
2015-03-22 13:18:31 -0300

    Adds API v1.1 messages endpoints

--- zaqar/api/v1_1/endpoints.py
+++ zaqar/api/v1_1/endpoints.py
@@ -17,7 +17,6 @@ from zaqar.common.api import utils as api_utils
 from zaqar.i18n import _
 import zaqar.openstack.common.log as logging
 from zaqar.storage import errors as storage_errors
-from zaqar.transport import utils
 from zaqar.transport import validation
 
 LOG = logging.getLogger(__name__)
@@ -36,6 +35,7 @@ class Endpoints(object):
 
         self._validate = validate
 
+    # Queues
     @api_utils.raises_conn_error
     def queue_list(self, req):
         """Gets a list of queues
@@ -56,10 +56,10 @@ class Endpoints(object):
             kwargs['marker'] = req._body.get('marker')
 
         if req._body.get('limit') is not None:
-            kwargs['limit'] = req._body.get('limit')
+            kwargs['limit'] = int(req._body.get('limit'))
 
         if req._body.get('detailed') is not None:
-            kwargs['detailed'] = req._body.get('detailed')
+            kwargs['detailed'] = 'true' == req._body.get('detailed')
 
         try:
             self._validate.queue_listing(**kwargs)
@@ -198,6 +198,10 @@ class Endpoints(object):
         project_id = req._headers.get('X-Project-ID')
         queue_name = req._body.get('queue_name')
 
+        LOG.debug(u'Queue get stats - queue: %(queue)s, '
+                  u'project: %(project)s',
+                  {'queue': queue_name, 'project': project_id})
+
         try:
             resp_dict = self._queue_controller.stats(queue_name,
                                                      project=project_id)
@@ -224,3 +228,389 @@ class Endpoints(object):
             headers = {'status': 200}
             resp = response.Response(req, body, headers)
             return resp
+
+    # Messages
+    @api_utils.raises_conn_error
+    def message_list(self, req):
+        """Gets a list of messages on a queue
+
+        :param req: Request instance ready to be sent.
+        :type req: `api.common.Request`
+        :return: resp: Response instance
+        :type: resp: `api.common.Response`
+        """
+        client_uuid = req._headers.get('Client-ID')
+        project_id = req._headers.get('X-Project-ID')
+        queue_name = req._body.get('queue_name')
+
+        LOG.debug(u'Message list - queue: %(queue)s, '
+                  u'project: %(project)s',
+                  {'queue': queue_name, 'project': project_id})
+
+        kwargs = {}
+
+        if req._body.get('marker') is not None:
+            kwargs['marker'] = req._body.get('marker')
+
+        if req._body.get('limit') is not None:
+            kwargs['limit'] = int(req._body.get('limit'))
+
+        if req._body.get('echo') is not None:
+            kwargs['echo'] = 'true' == req._body.get('echo')
+
+        if req._body.get('include_claimed') is not None:
+            kwargs['include_claimed'] = ('true' ==
+                                         req._body.get('include_claimed'))
+
+        try:
+            self._validate.message_listing(**kwargs)
+            results = self._message_controller.list(
+                queue_name,
+                project=project_id,
+                client_uuid=client_uuid,
+                **kwargs)
+
+            # Buffer messages
+            cursor = next(results)
+            messages = list(cursor)
+
+        except validation.ValidationFailed as ex:
+            LOG.debug(ex)
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers)
+        except storage_errors.DoesNotExist as ex:
+            LOG.debug(ex)
+            headers = {'status': 404}
+            return api_utils.error_response(req, ex, headers)
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Messages could not be listed.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+
+        if not messages:
+            messages = []
+
+        else:
+            # Found some messages, so prepare the response
+            kwargs['marker'] = next(results)
+            messages = [api_utils.format_message(message)
+                        for message in messages]
+
+        headers = {'status': 200}
+        body = {'messages': messages}
+
+        resp = response.Response(req, body, headers)
+        return resp
+
+    @api_utils.raises_conn_error
+    def message_get(self, req):
+        """Gets a message from a queue
+
+        :param req: Request instance ready to be sent.
+        :type req: `api.common.Request`
+        :return: resp: Response instance
+        :type: resp: `api.common.Response`
+        """
+        project_id = req._headers.get('X-Project-ID')
+        queue_name = req._body.get('queue_name')
+        message_id = req._body.get('message_id')
+
+        LOG.debug(u'Message get - message: %(message)s, '
+                  u'queue: %(queue)s, project: %(project)s',
+                  {'message': message_id,
+                   'queue': queue_name,
+                   'project': project_id})
+        try:
+            message = self._message_controller.get(
+                queue_name,
+                message_id,
+                project=project_id)
+
+        except storage_errors.DoesNotExist as ex:
+            LOG.debug(ex)
+            headers = {'status': 404}
+            return api_utils.error_response(req, ex, headers)
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Message could not be retrieved.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+
+        # Prepare response
+        message = api_utils.format_message(message)
+
+        headers = {'status': 200}
+        body = {'messages': message}
+
+        resp = response.Response(req, body, headers)
+        return resp
+
+    @api_utils.raises_conn_error
+    def message_get_many(self, req):
+        """Gets a set of messages from a queue
+
+        :param req: Request instance ready to be sent.
+        :type req: `api.common.Request`
+        :return: resp: Response instance
+        :type: resp: `api.common.Response`
+        """
+        project_id = req._headers.get('X-Project-ID')
+        queue_name = req._body.get('queue_name')
+        message_ids = list(req._body('message_ids'))
+
+        LOG.debug(u'Message get - queue: %(queue)s, '
+                  u'project: %(project)s',
+                  {'queue': queue_name, 'project': project_id})
+
+        try:
+            self._validate.message_listing(limit=len(message_ids))
+            messages = self._message_controller.bulk_get(
+                queue_name,
+                message_ids=message_ids,
+                project=project_id)
+
+        except validation.ValidationFailed as ex:
+            LOG.debug(ex)
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers)
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Message could not be retrieved.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, error, headers, ex)
+
+        # Prepare response
+        messages = list(messages)
+        if not messages:
+            messages = []
+
+        messages = [api_utils.format_message(message)
+                    for message in messages]
+
+        headers = {'status': 200}
+        body = {'messages': messages}
+
+        resp = response.Response(req, body, headers)
+        return resp
+
+    @api_utils.raises_conn_error
+    def message_post(self, req):
+        """Post a set of messages to a queue
+
+        :param req: Request instance ready to be sent.
+        :type req: `api.common.Request`
+        :return: resp: Response instance
+        :type: resp: `api.common.Response`
+        """
+        client_uuid = req._headers.get('Client-ID')
+        project_id = req._headers.get('X-Project-ID')
+        queue_name = req._body.get('queue_name')
+
+        LOG.debug(u'Messages post - queue:  %(queue)s, '
+                  u'project: %(project)s',
+                  {'queue': queue_name, 'project': project_id})
+
+        try:
+            # Place JSON size restriction before parsing
+            self._validate.message_length(req.content_length)
+        except validation.ValidationFailed as ex:
+            LOG.debug(ex)
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers)
+
+        # Deserialize and validate the incoming messages
+        document = api_utils.deserialize(req.stream, req.content_length)
+
+        if 'messages' not in document:
+            ex = _(u'Invalid request.')
+            error = _(u'No messages were found in the request body.')
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers, error)
+
+        # FIXME(vkmc): Use default TTL
+        _message_post_spec = (
+            ('ttl', int, 300),
+            ('body', '*', None),
+        )
+        messages = api_utils.sanitize(document['messages'],
+                                      _message_post_spec,
+                                      doctype=api_utils.JSONArray)
+
+        try:
+            self._validate.message_posting(messages)
+
+            if not self._queue_controller.exists(queue_name, project_id):
+                self._queue_controller.create(queue_name, project=project_id)
+
+            message_ids = self._message_controller.post(
+                queue_name,
+                messages=messages,
+                project=project_id,
+                client_uuid=client_uuid)
+
+        except validation.ValidationFailed as ex:
+            LOG.debug(ex)
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers)
+        except storage_errors.DoesNotExist as ex:
+            LOG.debug(ex)
+            headers = {'status': 404}
+            return api_utils.error_response(req, ex, headers)
+        except storage_errors.MessageConflict as ex:
+            LOG.exception(ex)
+            error = _(u'No messages could be enqueued.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Messages could not be enqueued.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+
+        # Prepare the response
+        headers = {'status': 201}
+        ids_value = ','.join(message_ids)
+        body = {'message_ids': ids_value}
+
+        resp = response.Response(req, body, headers)
+        return resp
+
+    @api_utils.raises_conn_error
+    def message_delete(self, req):
+        """Delete a message from a queue
+
+        :param req: Request instance ready to be sent.
+        :type req: `api.common.Request`
+        :return: resp: Response instance
+        :type: resp: `api.common.Response`
+        """
+        project_id = req._headers.get('X-Project-ID')
+        queue_name = req._body.get('queue_name')
+        message_id = req._body.get('message_id')
+
+        LOG.debug(u'Messages item DELETE - message: %(message)s, '
+                  u'queue: %(queue)s, project: %(project)s',
+                  {'message': message_id,
+                   'queue': queue_name,
+                   'project': project_id})
+
+        claim_id = req._body.get('claim_id')
+
+        try:
+            self._message_controller.delete(
+                queue_name,
+                message_id=message_id,
+                project=project_id,
+                claim=claim_id)
+        except storage_errors.MessageNotClaimed as ex:
+            LOG.debug(ex)
+            error = _(u'A claim was specified, but the message '
+                      u'is not currently claimed.')
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers, error)
+        except storage_errors.ClaimDoesNotExist as ex:
+            LOG.debug(ex)
+            error = _(u'The specified claim does not exist or '
+                      u'has expired.')
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers, error)
+
+        except storage_errors.NotPermitted as ex:
+            LOG.debug(ex)
+            error = _(u'This message is claimed; it cannot be '
+                      u'deleted without a valid claim ID.')
+            headers = {'status': 403}
+            return api_utils.error_response(req, ex, headers, error)
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Message could not be deleted.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+
+        headers = {'status': 204}
+        body = {}
+
+        resp = response.Response(req, body, headers)
+        return resp
+
+    @api_utils.raises_conn_error
+    def message_delete_many(self, req):
+        """Deletes a set of messages from a queue
+
+        :param req: Request instance ready to be sent.
+        :type req: `api.common.Request`
+        :return: resp: Response instance
+        :type: resp: `api.common.Response`
+        """
+        project_id = req._headers.get('X-Project-ID')
+        queue_name = req._body.get('queue_name')
+
+        LOG.debug(u'Messages collection DELETE - queue: %(queue)s, '
+                  u'project: %(project)s',
+                  {'queue': queue_name, 'project': project_id})
+
+        message_ids = list(req._body.get('message_ids'))
+        pop_limit = int(req._body.get('pop'))
+
+        try:
+            self._validate.message_deletion(message_ids, pop_limit)
+
+        except validation.ValidationFailed as ex:
+            LOG.debug(ex)
+            headers = {'status': 400}
+            return api_utils.error_response(req, ex, headers)
+
+        if message_ids:
+            return self._delete_messages_by_id(req, queue_name, message_ids,
+                                               project_id)
+        elif pop_limit:
+            return self._pop_messages(req, queue_name, project_id, pop_limit)
+
+    @api_utils.raises_conn_error
+    def _delete_messages_by_id(self, req, queue_name, ids, project_id):
+        try:
+            self._message_controller.bulk_delete(
+                queue_name,
+                message_ids=ids,
+                project=project_id)
+
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Messages could not be deleted.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+
+        headers = {'status': 204}
+        body = {}
+
+        resp = response.Response(req, body, headers)
+        return resp
+
+    @api_utils.raises_conn_error
+    def _pop_messages(self, req, queue_name, project_id, pop_limit):
+        try:
+            LOG.debug(u'POP messages - queue: %(queue)s, '
+                      u'project: %(project)s',
+                      {'queue': queue_name, 'project': project_id})
+
+            messages = self._message_controller.pop(
+                queue_name,
+                project=project_id,
+                limit=pop_limit)
+
+        except Exception as ex:
+            LOG.exception(ex)
+            error = _(u'Messages could not be popped.')
+            headers = {'status': 500}
+            return api_utils.error_response(req, ex, headers, error)
+
+        # Prepare response
+        if not messages:
+            messages = []
+
+        headers = {'status': 200}
+        body = {'messages': messages}
+
+        resp = response.Response(req, body, headers)
+        return resp
--- zaqar/common/api/utils.py
+++ zaqar/common/api/utils.py
@@ -14,12 +14,99 @@
 
 import functools
 
+import zaqar.common.api.errors as api_errors
 import zaqar.common.api.response as response
 from zaqar.i18n import _
 import zaqar.openstack.common.log as logging
 from zaqar.transport import utils
 
+JSONObject = dict
+"""Represents a JSON object in Python."""
+
+JSONArray = list
+"""Represents a JSON array in Python."""
+
 LOG = logging.getLogger(__name__)
+
+
+def deserialize(stream, len):
+    """Deserializes JSON from a file-like stream.
+
+    This function deserializes JSON from a stream, including
+    translating read and parsing errors to generic error types.
+
+    :param stream: file-like object from which to read an object or
+        array of objects.
+    :param len: number of bytes to read from stream
+    :raises: BadRequest, ServiceUnavailable
+    """
+
+    if len is None:
+        description = _(u'Request body can not be empty')
+        raise api_errors.BadRequestBody(description)
+
+    try:
+        # TODO(kgriffs): read_json should stream the resulting list
+        # of messages, returning a generator rather than buffering
+        # everything in memory (bp/streaming-serialization).
+        return utils.read_json(stream, len)
+
+    except utils.MalformedJSON as ex:
+        LOG.debug(ex)
+        description = _(u'Request body could not be parsed.')
+        raise api_errors.BadRequestBody(description)
+    except utils.OverflowedJSONInteger as ex:
+        LOG.debug(ex)
+        description = _(u'JSON contains integer that is too large.')
+        raise api_errors.BadRequestBody(description)
+    except Exception as ex:
+        # Error while reading from the network/server
+        LOG.exception(ex)
+        description = _(u'Request body could not be read.')
+        raise api_errors.ServiceUnavailable(description)
+
+
+def sanitize(document, spec=None, doctype=JSONObject):
+    """Validates a document and drops undesired fields.
+
+    :param document: A dict to verify according to `spec`.
+    :param spec: (Default None) Iterable describing expected fields,
+        yielding tuples with the form of:
+
+            (field_name, value_type, default_value)
+
+        Note that value_type may either be a Python type, or the
+        special string '*' to accept any type. default_value is the
+        default to give the field if it is missing, or None to require
+        that the field be present.
+
+        If spec is None, the incoming documents will not be validated.
+    :param doctype: type of document to expect; must be either
+        JSONObject or JSONArray.
+    :raises: DocumentTypeNotSupported, TypeError
+    :returns: A sanitized, filtered version of the document. If the
+        document is a list of objects, each object will be filtered
+        and returned in a new list. If, on the other hand, the document
+        is expected to contain a single object, that object's fields will
+        be filtered and the resulting object will be returned.
+    """
+
+    if doctype is JSONObject:
+        if not isinstance(document, JSONObject):
+            raise api_errors.DocumentTypeNotSupported()
+
+        return document if spec is None else filter(document, spec)
+
+    if doctype is JSONArray:
+        if not isinstance(document, JSONArray):
+            raise api_errors.DocumentTypeNotSupported()
+
+        if spec is None:
+            return document
+
+        return [filter(obj, spec) for obj in document]
+
+    raise TypeError('doctype must be either a JSONObject or JSONArray')
 
 
 def raises_conn_error(func):
@@ -46,3 +133,12 @@ def error_response(req, exception, headers=None, error=None):
     body = {'exception': exception, 'error': error}
     resp = response.Response(req, body, headers)
     return resp
+
+
+def format_message(message):
+    return {
+        'id': message['id'],
+        'ttl': message['ttl'],
+        'age': message['age'],
+        'body': message['body'],
+    }
